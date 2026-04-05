acb429572264cb33e88dee88512d9270bfb0d949
Milton <softwareandapp.arg@gmail.com>
2026-04-05 19:28:52 -0300

    colors

--- server.js
+++ server.js
@@ -736,7 +736,7 @@ VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)
         size,
         stock,
         extra,
-        JSON.stringify(colors || []), 
+        colors || [], 
         category,
         is_offer === "true" || is_offer === true
       ]
