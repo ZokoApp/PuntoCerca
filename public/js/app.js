a1e5c86cbc6ba98c1ee23bfa0a380765f7cf2dab
ZokoApp <softwareandapp.arg@gmail.com>
2026-04-11 16:57:36 -0300

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -58,3 +58,39 @@ if(searchInput){
     }
   });
 }
+
+export function showToast(message, type = "success") {
+
+  const colors = {
+    success: "bg-green-500",
+    error: "bg-red-500",
+    warning: "bg-yellow-500",
+    info: "bg-blue-500"
+  };
+
+  const toast = document.createElement("div");
+
+  toast.className = `
+    ${colors[type]} 
+    text-white px-5 py-3 rounded-lg shadow-lg 
+    flex items-center gap-3 
+    animate-slideIn
+  `;
+
+  toast.innerHTML = `
+    <span>${message}</span>
+    <button class="ml-auto font-bold">✕</button>
+  `;
+
+  document.getElementById("toastContainer").appendChild(toast);
+
+  // cerrar manual
+  toast.querySelector("button").onclick = () => {
+    toast.remove();
+  };
+
+  // auto remove
+  setTimeout(() => {
+    toast.remove();
+  }, 4000);
+}
