308d917618330a1334600c4fc56e9fdfe52ab62d
ZokoApp <softwareandapp.arg@gmail.com>
2026-04-14 20:42:22 -0300

    Update store-ui.js

--- public/js/store-ui.js
+++ public/js/store-ui.js
@@ -1,21 +1,4 @@
-function renderStore(store){
 
-  document.getElementById("storeName").innerText = store.name;
-
-  document.getElementById("storeDescription").innerText =
-    store.description || "";
-
-  if(store.cover_url){
-    document.getElementById("storeCover").style.backgroundImage =
-      `url('${store.cover_url}')`;
-  }
-
-  if(store.logo_url){
-    document.getElementById("storeLogo").style.backgroundImage =
-      `url('${store.logo_url}')`;
-  }
-
-}
 
 document.addEventListener("DOMContentLoaded", () => {
 
