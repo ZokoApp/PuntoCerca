a9ac7ee4c57443c9f9b0e1e2afeb3edfab7382df
ZokoApp <softwareandapp.arg@gmail.com>
2026-04-13 23:49:43 -0300

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,7 @@
 import { loadStores, focusStore } from './modules/stores.js';
 import { initSliders } from './modules/ui.js';
 import { loadProducts, loadOffers } from './modules/offers.js';
+import { CATEGORIES } from './data/categories.js';
 
 
 // GLOBAL (para HTML onclick)
