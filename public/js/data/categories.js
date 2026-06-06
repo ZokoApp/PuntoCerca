fb7ebf9eff5110e2f54cbd3913303c9854dcbc7a
ZokoApp <softwareandapp.arg@gmail.com>
2026-06-06 09:49:16 -0300

    Update categories.js

--- public/js/data/categories.js
+++ public/js/data/categories.js
@@ -158,6 +158,7 @@ export const CATEGORIES = {
 
   "Eventos": [
     { id: 43, name: "Salón de eventos" },
+     { id: 114, name: "SUM" },
          { id: 99, name: "Show en vivo" },
     { id: 44, name: "Catering" },
     { id: 45, name: "Fotografía" }
